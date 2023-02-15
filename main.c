5eec443b5afc40d60d0b6de98136967c8e084244
Connor Fricke <113947184+FXDuke@users.noreply.github.com>
2023-02-15 09:50:20 -0500

    Organization, MoveSnake() & snake variables

--- main.c
+++ main.c
@@ -1,17 +1,46 @@
+
+
 // Global Variables
+
+// LED Variables
 
 int ScreenSize_X = 200;
 int ScreenSize_Y = 200;
-
-bool ScreenMap[ScreenSize_Y][ScreenSize_X][1] = {
-  {{true},{false}...}, // add all digital signals to each x here
-};
 
 int ScreenMap_LED[ScreenSize_Y][ScreenSize_X] = {
   {{1,4,7,10},{2,5,8,11}...}, // screen map for getting the pin number of the led
 };
 
 bool SCREENCHANGED = false; // Change to true when turning LED on, this will make it so LED's are not being updated when nothing has been changed
+// for buttons
+
+bool ScreenMap[ScreenSize_Y][ScreenSize_X][1] = {
+  {{true},{false}...}, // add all digital signals to each x here
+};
+
+// Snake Variables 
+
+bool Gameplay = false;
+int Speed = 1;
+int Direction = 1; // or -1 
+bool Up = false;
+int CurrentSize = 1;
+int Size[ScreenSize_Y*ScreenSize_X][2]; // Snake's max size is the size of the display
+Size[0] = {round(ScreenSize_X/2),round(ScreenSize_Y/2)};
+
+// Button Variables
+
+int ControlSize = 4; // up down left right (for now)
+int ButtonCooldown = 0;
+int Minimum_Button_Read = 1000;
+int ButtonCooldown_Amount = 200;
+
+int ButtonMap[ControlSize] = {2,3,4,5}; // analog pins here
+
+// Refresh Rate
+
+int _RefreshRate;
+int _OldRate = millis();
 
 // LED Manager
 
@@ -23,11 +52,12 @@ void iterateLED(void (*returnFunction)(int,int)) {
   }
 }
 
-void ScreenOff() {
+void screenOff() {
   void TurnOff(int Y, int X) {
     ScreenMap[Y][X][0] = false; 
   }
   iterateLED(TurnOff);
+  SCREENCHANGED = true;
 }
 
 void screenBorder() {
@@ -41,6 +71,7 @@ void screenBorder() {
       ScreenMap[Index_Y][ScreenSize_X-1][0] = true;
     }
   }
+  SCREENCHANGED = true;
 }
 
 void refreshScreen() {
@@ -62,17 +93,64 @@ void updateLED(int X,int Y, bool Value) {
 
 // Button Manager
 
-// wip
+int[2] buttonDown() {
+  int greatest = 0;
+  int FinalPin;
+  for (int Index = 0; Index < ControlSize; Index++) {
+    int Read = analogRead(ButtonMap[Index]);
+    if (Read > greatest) {
+      greatest = Read;
+      FinalPin = Index;
+    }
+  }
+  return FinalPin,greatest;
+}
+
+// Snake Movements
+
+void MoveSnake() {
+  updateLED(Size[0][0],Size[0][1],false);
+  if (Up == true) {
+    Size[0][1] += Direction;
+  } else {
+    Size[0][0] += Direction;
+  }
+  updateLED(Size[0][0],Size[0][1],true);
+  for (int Index = 0; Index < CurrentSize; Index++) {
+    updateLED(Size[Index][0],Size[Index][1],false);
+    Size[Index] = Size[Index-1];
+    updateLED(Size[Index][0],Size[Index][1],true);
+  }
+}
 
 // Main Functions 
 
 void setup() {
   Serial.begin(9600);
+  screenBorder();
+  updateLED(round(ScreenSize_X/2),round(ScreenSize_Y/2),true); // snake start position
 }
 
 void loop() {
+  
+  _RefreshRate = millis()-_OldRate;
+  _OldRate = _RefreshRate;
+  
   if (SCREENCHANGED == true) {
     refreshScreen();
   }
+  
+  if (ButtonCooldown == 0) {
+    int ButtonHeld[2] = ButtonDown();
+    if (ButtonHeld[1]>Minimum_Button_Read) {
+      // add function stuff here
+      
+      ButtonCooldown = ButtonCooldown_Amount;
+    }
+  } else {
+    ButtonCooldown = (ButtonCooldown-_RefreshRate>0) ? ButtonCooldown-_RefreshRate : 0;
+  }
+  
+  delay(10);
 }
 
